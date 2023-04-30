cbd8a93f7cfa5d84d9fc3ea67407ee846bebd560
matthieuy <matthieu.yourkevitch@ac-clermont.fr>
2023-04-30 10:35:18 +0200

    sudoers : ajout du reboot/halt sans mdp

--- scripts/upgrade/2023043000.sh
+++ scripts/upgrade/2023043000.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Script vierge pour les upgrades (fichier à nommer avec ${VERSION_SCRIPT})
+# 2023043000 : Ajout du fichier /etc/sudoers.d/reboot qui permet le reboot/halt sans mdp
 
 source /home/pi/voron/modules/_common.sh
 
@@ -14,22 +14,22 @@ if [ '${whoami}' == 'root' ]; then
     #chmod +x /etc/cron.d/voron-cron
 
     # Màj Sudoers
-    #_logUpgrade "Màj sudoers"
-    #cp -f ${SCRIPT_DIR}/conf/etc/sudoers.d/* /etc/sudoers.d/
-    #chmod +x /etc/sudoers.d/*
+    _logUpgrade "Màj sudoers (ajout du reboot)"
+    cp -f ${SCRIPT_DIR}/conf/etc/sudoers.d/* /etc/sudoers.d/
+    chmod +x /etc/sudoers.d/*
 
     #_logUpgrade "Fin de l'upgrade (en root)"
     #_logUpgrade "Aucune action root pour cette mise à jour"
 
     # Necessite un reboot
-    #echo "1" > ${NEED_REBOOT_UPGRADE}
+    echo "1" > ${NEED_REBOOT_UPGRADE}
 else 
 ###################
 # Upgrade en "pi" #
 ###################
 
     # Necessite un reboot
-    #echo "1" > ${NEED_REBOOT_UPGRADE}
+    echo "1" > ${NEED_REBOOT_UPGRADE}
 
     #_logUpgrade "Fin de l'upgrade (utilisateur standard)"
 fi
